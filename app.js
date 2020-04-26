86c5fada15d4bf6c5d73a18fd15648fc73e1b41e
Oriana Ruiz <oriru92@gmail.com>
2020-04-26 19:10:19 -0300

    Dotenv package install and configuration.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,4 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+dotenv.config();
+
+const PORT = process.env.PORT || '5000';
+
+app.listen(PORT, function() {
+    console.log('Server listening at http://localhost:' + PORT);
+});
